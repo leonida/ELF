cae83ef93335a43f17238a40ed097243497664ef
lirongda <leonda@yeah.net>
2021-08-12 17:39:32 +0800

    更新画图程序

--- accuracy/da.py
+++ accuracy/da.py
@@ -2,7 +2,7 @@ import pandas as pd
 import re
 import numpy as np
 from matplotlib import pyplot as plt
-# file = pd.read_csv("./da.csv", error_bad_lines=False)
+
 file = pd.read_csv("./accuracy/da.txt", error_bad_lines=False)
 train = pd.DataFrame(file)
 count = 0
@@ -10,10 +10,11 @@ opt = []
 
 # 将best_win_rate输出到list 保存y值
 for i in range(len(train)):
+    # 读取每一行找到每轮迭代的最佳准确率
     if re.findall("best_win_rate*", train.loc[i][0]):
         print(count)
         count += 1
-        print(train.loc[i][0])
+        # 读取准确率的值并转化为float类型
         opt.append(float(train.loc[i][0][14:]))
 
 for i in opt:
